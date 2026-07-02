75218d1e5efe00156f1bd7e4e0ab6179b28a1e88
មុនី ឫទ្ធិ <myidea1311@gmail.com>
2026-07-02 09:44:01 +0700

    fix can install as app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'logo.png'],
+      manifest: {
+        name: 'Attendance Management System',
+        short_name: 'Attendance',
+        description: 'Attendance Management System',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
+  ],
 })
